386463229d357ac4cc813381e5464a0a95009778
Ive <ive.alex.89@gmail.com>
2023-04-23 17:09:35 -0400

    New Commit

--- Module 8 Practice App/Program.cs
+++ Module 8 Practice App/Program.cs
@@ -1,9 +1,42 @@
 ﻿using System;
 using System.IO;
 
-namespace DirectoryManager
+namespace FolderPurge
 {
-  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paste the link to the Folder you would like to clean:");
+            string? Link = Console.ReadLine();
+
+            if (Directory.Exists(Link))
+            {
+                foreach (string link in Directory.GetFiles(Link))
+                {
+                    try
+                    {
+                        DirectoryInfo dirInfo = new DirectoryInfo(Link);
+
+                        if(dirInfo.LastAccessTime > DateTime.Now.Subtract(TimeSpan.FromMinutes(30)))
+                        { dirInfo.Delete(true); }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadKey();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Directory Doesnt Exist");
+            }
+        }
+    }
+}
 
 
-       
+
+
+
