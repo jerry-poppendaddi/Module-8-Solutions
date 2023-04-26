65301fb71ee6f9ebf7f34c4d1a8d62bd425d248c
Ive <ive.alex.89@gmail.com>
2023-04-25 23:58:59 -0400

    New Commit

--- Module 8 Task 4/Program.cs
+++ Module 8 Task 4/Program.cs
@@ -1,1 +1,96 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+   
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string Group { get; set; }
+
+        //Constructor
+        public Student(string name, DateTime dateOfBirth, string group)
+        {
+            Name = Name;
+            Group = Group;
+            DateOfBirth = dateOfBirth;
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide a path to your Desktop:");
+            string? filePath = Console.ReadLine();
+            Console.WriteLine("Provide a path to your .dat file:");
+            string? datfilePath = Console.ReadLine();
+            
+            //Creating a new folder on desktop named "Students"
+            DirectoryInfo dirInfo = new DirectoryInfo(filePath);
+            dirInfo.CreateSubdirectory("Students");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            //DeSerializing .dat file
+            using (var fs = new FileStream(datfilePath, FileMode.OpenOrCreate))
+            {
+                Student[] students = (Student[])formatter.Deserialize(fs);
+                for (int i = 0; i < students.Length; i++)
+                {
+                    string a = filePath + "/" + students[i].Group + ".txt";
+                    using (FileStream filestream = new FileStream(a, FileMode.Append))
+                    {
+                        using (StreamWriter sw = new StreamWriter(filestream))
+                        {
+                            sw.WriteLine($"{students[i].Name}, {students[i].DateOfBirth}");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+      
+
+
+
+       
+
+           
+            
+            
+            
+            
+            
+            
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+         
+   
